df2db1e29540c1599d935c7e75efd4f13cc53b92
Patrick Lawrence <patrick.lawrence@gmail.com>
2024-05-22 21:28:15 -0700

    Add a script to set NAS ownership and permissions

---
+++ roles/nas/files/ownership.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+OWNERSHIP=/opt/etc/ownership.json
+
+if [ ! -f ${OWNERSHIP} ]; then
+  echo "ownership file, ${OWNERSHIP}, does not exist"
+  exit 1
+fi
+
+for row in $( cat ${OWNERSHIP} | jq -r '.[] | @base64' ); do
+  ACL_USER=$(echo ${row} | base64 --decode | jq -r '.acl_user // ""' )
+  ACL_USER_PERMS=$(echo ${row} | base64 --decode | jq -r '.acl_user_perms // "rwX"' )
+  ACL_GROUP=$(echo ${row} | base64 --decode | jq -r '.acl_group // ""' )
+  ACL_GROUP_PERMS=$(echo ${row} | base64 --decode | jq -r '.acl_group_perms // "rwX"' )
+  UNIX_USER=$(echo ${row} | base64 --decode | jq -r '.unix_user // ""' )
+  UNIX_GROUP=$(echo ${row} | base64 --decode | jq -r '.unix_group // ""' )
+  UNIX_PERMS_DIR=$(echo ${row} | base64 --decode | jq -r '.unix_perms_dir // "0755"' )
+  UNIX_PERMS_FILE=$(echo ${row} | base64 --decode | jq -r '.unix_perms_file // "0644"' )
+  PATH_NAME=$(echo ${row} | base64 --decode | jq -r '.path' )
+done
+
+if [[ -n ${ACL_USER} || -n ${ACL_GROUP} || -n ${PATH_NAME} ]]; then
+  setfacl -Rm "u:${ACL_USER}:${ACL_USER_PERMS},g:${ACL_GROUP}:${ACL_GROUP_PERMS}" "${PATH_NAME}"
+  setfacl -Rdm "u:${ACL_USER}:${ACL_USER_PERMS},g:${ACL_GROUP}:${ACL_GROUP_PERMS}" "${PATH_NAME}"
+fi
+
+if [[ -n ${UNIX_USER} || -n ${UNIX_GROUP} || -n ${PATH_NAME} ]]; then
+  find "${PATH_NAME}" \( \! -user "${UNIX_USER}" -o \! -group "${UNIX_GROUP}" \) -print0 | xargs -r -n 10 -0 chown "${UNIX_USER}:${UNIX_GROUP}"
+  find "${PATH_NAME}" \( -type d -a \! -perm "${UNIX_PERMS_DIR}" \) -print0 | xargs -r -n 10 -0 chmod "${UNIX_PERMS_DIR}"
+  find "${PATH_NAME}" \( -type f -a \! -perm "${UNIX_PERMS_FILE}" \) -print0 | xargs -r -n 10 -0 chmod "${UNIX_PERMS_FILE}"
+fi
--- roles/nas/vars/README.md
+++ roles/nas/vars/README.md
@@ -17,3 +17,19 @@ smb_shares:
     path: /path/to/share
     timemachine: true      # default is omitted
 ```
+
+Ownership paramters for directory paths:
+
+```yaml
+ownerships:
+  - acl_user: username
+    acl_user_perms: rwX     # optional, default is "rwX"
+    acl_group: groupname
+    acl_group_perms: rwX    # optional, default is "rwX"
+    unix_user: username
+    unix_group: groupname
+    unix_perms_dir: "0755"  # optional, default is "0775"
+    unix_perms_file: "0755" # optional, default is "0664"
+    path: /tank/path/name
+ 
+```
