3927cf44a0b89adc847286b738697ea90dc45443
Patrick Lawrence <patrick.lawrence@gmail.com>
2024-05-27 12:13:58 -0700

    Convert tdarr to Ubuntu 24.04/NFSv4, fixes - Convert tdarr node containers to Ubuntu 24.04 and NFSv4 - Add a username based on the vmid for tdarr nodes.   This was necessary so that the user keytab could be pulled. - Convert tdarr server container to Ubuntu 24.04 and NFSv4 - Have the tdarr server run as a non-root user - Downgrade the tdarr version (the latest was not working) - Add more memory to sabnzbd as a test since the process was restarting - Fix the ownership script to actually act upon all entries - When adding a user to an idm group, append it rather than replace - Process the passed in state in the systemd template task

--- roles/nas/files/ownership.sh
+++ roles/nas/files/ownership.sh
@@ -19,15 +19,15 @@ for row in $( cat ${OWNERSHIP} | jq -r '.[] | @base64' ); do
   UNIX_PERMS_DIR=$(echo ${row} | base64 --decode | jq -r '.unix_perms_dir // "0755"' )
   UNIX_PERMS_FILE=$(echo ${row} | base64 --decode | jq -r '.unix_perms_file // "0644"' )
   PATH_NAME=$(echo ${row} | base64 --decode | jq -r '.path' )
+
+  if [[ -n ${ACL_USER} || -n ${ACL_GROUP} || -n ${PATH_NAME} ]]; then
+    setfacl -Rm "u:${ACL_USER}:${ACL_USER_PERMS},g:${ACL_GROUP}:${ACL_GROUP_PERMS}" "${PATH_NAME}"
+    setfacl -Rdm "u:${ACL_USER}:${ACL_USER_PERMS},g:${ACL_GROUP}:${ACL_GROUP_PERMS}" "${PATH_NAME}"
+  fi
+
+  if [[ -n ${UNIX_USER} || -n ${UNIX_GROUP} || -n ${PATH_NAME} ]]; then
+    find "${PATH_NAME}" \( \! -user "${UNIX_USER}" -o \! -group "${UNIX_GROUP}" \) -print0 | xargs -r -n 10 -0 chown "${UNIX_USER}:${UNIX_GROUP}"
+    find "${PATH_NAME}" \( -type d -a \! -perm "${UNIX_PERMS_DIR}" \) -print0 | xargs -r -n 10 -0 chmod "${UNIX_PERMS_DIR}"
+    find "${PATH_NAME}" \( -type f -a \! -perm "${UNIX_PERMS_FILE}" \) -print0 | xargs -r -n 10 -0 chmod "${UNIX_PERMS_FILE}"
+  fi
 done
-
-if [[ -n ${ACL_USER} || -n ${ACL_GROUP} || -n ${PATH_NAME} ]]; then
-  setfacl -Rm "u:${ACL_USER}:${ACL_USER_PERMS},g:${ACL_GROUP}:${ACL_GROUP_PERMS}" "${PATH_NAME}"
-  setfacl -Rdm "u:${ACL_USER}:${ACL_USER_PERMS},g:${ACL_GROUP}:${ACL_GROUP_PERMS}" "${PATH_NAME}"
-fi
-
-if [[ -n ${UNIX_USER} || -n ${UNIX_GROUP} || -n ${PATH_NAME} ]]; then
-  find "${PATH_NAME}" \( \! -user "${UNIX_USER}" -o \! -group "${UNIX_GROUP}" \) -print0 | xargs -r -n 10 -0 chown "${UNIX_USER}:${UNIX_GROUP}"
-  find "${PATH_NAME}" \( -type d -a \! -perm "${UNIX_PERMS_DIR}" \) -print0 | xargs -r -n 10 -0 chmod "${UNIX_PERMS_DIR}"
-  find "${PATH_NAME}" \( -type f -a \! -perm "${UNIX_PERMS_FILE}" \) -print0 | xargs -r -n 10 -0 chmod "${UNIX_PERMS_FILE}"
-fi
